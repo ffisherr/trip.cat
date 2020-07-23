ffabeccc5ca2ccc222421efb6d0ccd4eafc91d53
sseklecov <p0o9i8u7!>
2020-07-23 19:57:42 +0700

    rename Organizer

--- app/models.py
+++ app/models.py
@@ -2,24 +2,24 @@ from app import db
 
 
 class User(db.Model):
-	id    = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(64), unique=True)
-	trips = db.relationship('Trip', backref='user', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True)
+    trips = db.relationship('Trip', backref='user', lazy='dynamic')
 
 
 class Trip(db.Model):
-	id  = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), unique=True)
-	description = db.Column(db.Text)
-	user_id = db.Column(db.Integer,   db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-class Orginizer(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(64), unique=True)
+class Organizer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True)
 
 
 class Partner(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(64), unique=True)
-	rate = db.Column(db.Integer) 
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True)
+    rate = db.Column(db.Integer)
