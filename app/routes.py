9234eb5bcf522f70e90b57cf567e50d253027890
sseklecov <p0o9i8u7!>
2020-07-23 22:11:25 +0700

    pages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from flask import render_template
+
 from app import app
+
 
 @app.route('/')
 def index():
-	return '<h1>Hello</h1>'
+    return render_template('base.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
